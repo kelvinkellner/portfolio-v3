bf3301c34b3901b208cf02ae3b263c3015c01ae5
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-03-25 19:11:31 -0400

    Trying wacky design stuff

---
+++ src/components/FunWelcome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Row from 'react-bootstrap/Row';
+
+class FunWelcome extends Component { 
+    render() {
+        return (
+            <Row>
+                <h1>Hi there, my name is Kelvin!</h1>
+            </Row>
+        );
+    }
+}
+
+export default FunWelcome;
