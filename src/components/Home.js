fe26f25a9e55d80912dc587fb93d2b442da2c5b4
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-03-25 12:21:57 -0400

    Improved formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,10 @@ class Home extends Component {
         return (
             <Container id="home">
                 <Row>
-                    <Col md={12} lg={9} style={{padding: 0}}>
-                        <h3 className="font-weight-bold">passionate about learning & creating<span>.</span></h3>
+                    <Col md={12} lg={8} style={{padding: 0, marginBottom: "3rem"}}>
+                        <h3 className="font-weight-bold heading">passionate about learning & creating<span>.</span></h3>
                         <p>
-                            <a href='javascript:smileyEasterEgg()'>Hello!</a> <svg id="smiley" style={{visibility: "hidden", margin: "-16px"}} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                            Hello! <svg id="smiley" style={{visibility: "hidden", margin: "-16px"}} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                             <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-12c.331 1.465 2.827 4 6.001 4 3.134 0 5.666-2.521 5.999-4zm0-3.998l-.755.506s-.503-.948-1.746-.948c-1.207 0-1.745.948-1.745.948l-.754-.506c.281-.748 1.205-2.002 2.499-2.002 1.295 0 2.218 1.254 2.501 2.002zm-7 0l-.755.506s-.503-.948-1.746-.948c-1.207 0-1.745.948-1.745.948l-.754-.506c.281-.748 1.205-2.002 2.499-2.002 1.295 0 2.218 1.254 2.501 2.002z"/></svg>
                             I am a software developer & graphic designer currently studying Computer Science & User-Experience Design at Wilfrid Laurier University. 
                             I have an affection for building new skills and helping others grow.</p>
